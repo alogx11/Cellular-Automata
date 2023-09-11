bd2bf4d464beecc865096af0b88a97cbb1c73fad
alogx11 <alogx11@gmail.com>
2023-09-11 15:21:18 -0500

    generates rows

--- sketch.js
+++ sketch.js
@@ -1,37 +1,44 @@
-﻿const CELLSIZE = 80;
-const ROWWIDTH = 600;
-const MAXGENERATIONS = 3;
+﻿const CELLSIZE = 4;
+let rowWidth = 100;
+let MAXGENERATIONS = 10;
 
-let rule = new Array(8);
+let rule = [0, 0, 0, 1, 1, 1, 1, 0];
 let generations;
 let cells;
 
 function setup() {
   createCanvas(windowWidth, windowHeight);
-  background(0);
-  rule = [0, 0, 0, 1, 1, 1, 1, 0];
+  background(255);
+  rowWidth = Math.floor(windowWidth / CELLSIZE);
+  print(rowWidth);
+  // rule = [0, 0, 0, 1, 1, 1, 1, 0];
   generations = 0;
-  cells = new Array(ROWWIDTH);
-  rule = [0, 0, 0, 1, 1, 1, 1, 0];
-  // noStroke();
-  rect(0 + CELLSIZE * i, 0 * CELLSIZE, CELLSIZE);
+  cells = new Array(rowWidth);
+  cells.fill(0);
+  cells[Math.floor(rowWidth / 2)] = 1;
+  noStroke();
+  /*
   while (generations < MAXGENERATIONS) {
-    print("18");
     displayCells();
-    // computeNextGeneration();
+    computeNextGeneration();
   }
+  */
+}
+
+function draw() {
+  frameRate(10);
+  displayCells();
+  computeNextGeneration();
 }
 
 function displayCells() {
   for (let i = 0; i < cells.length; i++) {
     // alive cell, fill black
-    if (cells[i] == 0) {
-      print("alive");
+    if (cells[i] == 1) {
       fill(0);
       rect(0 + CELLSIZE * i, generations * CELLSIZE, CELLSIZE);
     } else {
       // dead cell, white
-      print("ded");
       fill(255);
       rect(0 + CELLSIZE * i, generations * CELLSIZE, CELLSIZE);
     }
@@ -39,19 +46,22 @@ function displayCells() {
 }
 
 function computeNextGeneration() {
-  let size = cells.length;
-  let nextGenCells = new Array(size);
+  let nextGenCells = new Array(rowWidth);
   nextGenCells.fill(0);
-  for (let i = 0; i < cells.length - 2; i++) {
+  for (let i = 0; i < nextGenCells.length; i++) {
+    // off left edge, use right most as left bit
     if (i - 1 < 0) {
       nextGenCells[i] = applyRule(
         cells[cells.length - 1],
         cells[i],
         cells[i + 1]
       );
+      // off right edge, use left most as right bit
     } else if (i + 1 == cells.length) {
       nextGenCells[i] = applyRule(cells[i - 1], cells[i], cells[0]);
+      // in middle area
     } else {
+      print(cells);
       nextGenCells[i] = applyRule(cells[i - 1], cells[i], cells[i + 1]);
     }
   }
@@ -60,6 +70,13 @@ function computeNextGeneration() {
 }
 
 function applyRule(a, b, c) {
+  /*
+  print("a: " + a + " b: " + b + " c: " + c);
   let index = a * Math.pow(2, 2) + b * Math.pow(2, 1) + c * Math.pow(2, 0);
-  return rule[index];
+  print("ind " + index);
+  print(rule);
+  print(rule[index]);
+  */
+  return (a == 1) ^ ((b == 1) | (c == 1)) ? 1 : 0;
+  // return rule[index];
 }
